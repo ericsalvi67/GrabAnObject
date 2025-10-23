64ab618b85ca7c9990d6a6c5f42b8666989662be
ericsalvi67 <ericsalvi67@gmail.com>
2025-10-22 23:37:42 -0300

    🚧

--- src/Domain/Maintenance/MaintenanceQuery.java
+++ src/Domain/Maintenance/MaintenanceQuery.java
@@ -9,7 +9,7 @@ public class MaintenanceQuery {
         DataBaseConnectionManager conn = new DataBaseConnectionManager(1, "postgres", "postgres", "postgres");
 
         String sql = "INSERT INTO maintenance (id, user_id, object_id, service_type, description, performed_at, deleted, last_modification) " +
-                    "VALUES (" + maintenance.id + ", " + maintenance.user_id + ", " + maintenance.object_id + ", '" + maintenance.service_type + "', '" + maintenance.description + "', '" + maintenance.performed_at + "', " + maintenance.deleted + ", now())";
+                    "VALUES (" + maintenance.id + ", " + maintenance.user_id + ", " + maintenance.object_id + ", '" + maintenance.service_type + "', '" + maintenance.description + "', now(), " + maintenance.deleted + ", now())";
 
 		try{
 			conn.connectDataBase();
